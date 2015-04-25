0b0480cb530b9c6c75c95112fb6925ebe8fd494a
a554b554 <changxiao@acm.org>
2015-04-25 16:31:45 +0800

    Initial Commit

---
+++ lab5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab5
+//
+//  Created by DarkTango on 4/25/15.
+//  Copyright (c) 2015 DarkTango. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
